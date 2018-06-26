a691d0a0f46f36061dee7cc87c1438977e314fdf
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-26 18:34:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+}
